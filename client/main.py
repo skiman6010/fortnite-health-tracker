8064c19ae9a599444998ee0f6a2a84c414304b82
aman601 <aman601601@gmail.com>
2019-05-09 23:22:03 -0400

    Changed for melee.

--- client/main.py
+++ client/main.py
@@ -5,38 +5,22 @@ import pytesseract
 import math
 import requests
 import json
+import winsound
 
 config = json.loads(open('config.json').read())
 
 class Imaging:
     def __init__(self):
-        self.currentHealth = 100
+        self.currentHealth = 0
         self.ocrErr = False
 
     def process(self, greyImg):
         self.currentHealth
         txt = pytesseract.image_to_string(greyImg)
-        if not txt == '':
-            new_health = ''.join(filter(lambda x: x.isdigit(), txt))
-            if not new_health == '':
-                if new_health == '01':
-                    new_health = '0'
-                    print('Health: {}'.format(new_health))
-                else:
-                    print('Health: {}'.format(new_health))
-        try:
-            health = int(txt)
-            self.ocrErr = False
-        except:
-            health = self.currentHealth
-            if not self.ocrErr:
-                health = self.currentHealth - 1
-                self.ocrErr = True
-
-        if health < self.currentHealth:
-            self.currentHealth = health
-            return True
-        return False
+        if txt != self.currentHealth:
+            print(txt)
+            
+            self.currentHealth = txt
 
 def main():
     imaging = Imaging()
@@ -47,20 +31,25 @@ def main():
         offy = config['off_y']
         
         new_size_x = 300
-        new_size_y = 100
+        new_size_y = 300
 
-        img = ImageGrab.grab(bbox=(x, y, x + offx, y + offy)).convert('L')
-        img = img.resize((new_size_x, new_size_y), Image.ANTIALIAS)
+        img = ImageGrab.grab(bbox=(x-355, y+5, x + offx-220, y + offy+95)).convert('L')
+        #img = img.resize((new_size_x, new_size_y), Image.ANTIALIAS)
+        #img = np.float32(img)
+        #cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        #img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
         #helping with image processing
-        img = np.array(img)
+        
+        img = np.array(img, dtype = 'uint8')
+        
+        
         cv2.imshow('Fortnite Health', img)
         
         if imaging.process(img):
-            if not config['raspberry_pi_host'] is None:
-                requests.get(config['raspberry_pi_host'])
-
+            winsound.Beep(500,0)
         if cv2.waitKey(25) & 0xFF == ord('q'):  
             cv2.destroyAllWindows()
+            
             break
 
 if __name__ == '__main__':

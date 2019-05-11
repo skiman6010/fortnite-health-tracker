d952eb893fc26feea52c01d7e16febaebbc7998f
aman601 <aman601601@gmail.com>
2019-05-11 15:34:43 -0400

    Works now
    Only reads numbers

--- client/main.py
+++ client/main.py
@@ -8,19 +8,25 @@ import json
 import winsound
 
 config = json.loads(open('config.json').read())
-
+numtxt = 0
 class Imaging:
     def __init__(self):
         self.currentHealth = 0
+        numtxt = 0
         self.ocrErr = False
 
     def process(self, greyImg):
         self.currentHealth
+        global numtxt
         txt = pytesseract.image_to_string(greyImg)
-        if txt != self.currentHealth:
-            print(txt)
-            
-            self.currentHealth = txt
+        try:
+            numtxt = int(txt)
+        except ValueError:
+            winsound.Beep(37,0)
+        if numtxt != self.currentHealth:
+            print(numtxt)
+            winsound.PlaySound('yousuck.wav', winsound.SND_FILENAME)
+            self.currentHealth = numtxt
 
 def main():
     imaging = Imaging()
@@ -33,17 +39,17 @@ def main():
         new_size_x = 300
         new_size_y = 300
 
-        img = ImageGrab.grab(bbox=(x-355, y+5, x + offx-220, y + offy+95)).convert('L')
+        img = ImageGrab.grab(bbox=(x-375, y, x + offx-220, y + offy+105)).convert('L')
+        
         #img = img.resize((new_size_x, new_size_y), Image.ANTIALIAS)
         #img = np.float32(img)
         #cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         #img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
         #helping with image processing
-        
         img = np.array(img, dtype = 'uint8')
-        
-        
-        cv2.imshow('Fortnite Health', img)
+        #img = np.float32(img)
+        #img = cv2.blur(img, (9,9), img)
+        cv2.imshow('Fortnite Health', np.invert(img))
         
         if imaging.process(img):
             winsound.Beep(500,0)
